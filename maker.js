97d36483756bfdb754d2683eeba609c283c092ca
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-13 10:32:46 -0500

    got names to dom, much rejoicing

--- maker.js
+++ maker.js
@@ -1,11 +1,24 @@
 // Const that grabs article
 const citiesArticleRef = document.querySelector("#myCities")
 
-const citiesDomBuilder = (date) => {
+const citiesDomBuilder = () => {
 // for each iterates through object and citiies
     citiesDatabase.cities.forEach(currentCity => {
         // citySection creates section element for city info
         const citySection = document.createElement('section')
+        citiesArticleRef.appendChild(citySection)
+        
+        const cityName = document.createElement('h2')
+        cityName.textContent = currentCity.name 
+        citySection.appendChild(cityName)
+
+        const imageDom = document.createElement('img')
+        imageDom.textContent = currentCity.image 
+        cityName.appendChild(imageDom)
+
+        const topFive = document.createElement('ul')
+
+        const topFiveAttractions = document.createElement('li')
         
     });
 // Display your trips on the Dom, styled in cards. 
